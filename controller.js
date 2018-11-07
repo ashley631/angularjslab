0ba422fe11acbfcc0576fb798a36f92417d74d95
Ashley Hill <42178170+ashley631@users.noreply.github.com>
2018-11-06 22:26:05 -0500

    forgot to update controller.js
    update

--- controller.js
+++ controller.js
@@ -8,16 +8,21 @@ function TodoController () {
         {task : "Do Dishes", completed: false},
         {task : "Buy Groceries", completed: true},
     
-
 ];
 
+    vm.addTask = (taskToAdd)  => {
+     vm.tasks.push(angular.copy({task: taskToAdd, completed: false}));
     
+}
+
+     vm.removeTask = (index) => {
+         vm.tasks.splice(index, 1)
 
 }
 
-
-
+}
 
 angular
 .module("ToDoApp")
 .controller("TodoController", TodoController);
+
